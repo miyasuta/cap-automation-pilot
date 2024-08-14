15196f9c4c367b7908652d087892b4c6bd562daf
Mio Yasutake <mio.fujita.01@gmail.com>
2024-08-15 05:28:09 +0900

    implement triggerCommand action

--- srv/automation-pilot.cds
+++ srv/automation-pilot.cds
@@ -2,4 +2,5 @@ using { Executions_types } from './external/ExecutionsAPI';
 
 service AutomationPilotService {
     function getExecutions() returns array of Executions_types.execution;
+    function triggerCommand(commandId: String) returns Executions_types.execution;
 }
--- srv/automation-pilot.js
+++ srv/automation-pilot.js
@@ -3,14 +3,17 @@ const cds = require('@sap/cds')
 module.exports=class AutomationPilotService extends cds.ApplicationService{
     init(){
         this.on('getExecutions', async(req) => {
-            //https://cap.cloud.sap/docs/node.js/core-services#srv-send-request
             const executionApi = await cds.connect.to('Executions')
-            // const response = await executionApi.send('executions')
-            const response = await executionApi.send({
-                method: 'GET',
-                path: '/executions'
+            const response = await executionApi.send('GET', '/executions')
+            return response
+        })
+
+        this.on('triggerCommand', async(req) => {
+            const commandId = req.data.commandId
+            const executionApi = await cds.connect.to('Executions')
+            const response = await executionApi.send('POST', '/executions', {
+                commandId: commandId
             })
-            console.log("response:", response)
             return response
         })
         return super.init()
